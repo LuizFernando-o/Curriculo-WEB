7983e3934dae5ef5675efbc695627bcfe24cd914
luizfernando-o <luiz.2019230186@unicap.br>
2020-10-13 16:26:47 -0300

    primeiro commit

---
+++ CurriculoWEB/index.php
@@ -0,0 +1,9 @@
+<?php
+//header
+include_once "paginas/includes/header.php";
+
+//pagina
+include_once "paginas/curriculo.php";
+
+//footer
+include_once "paginas/includes/footer.php";
---
+++ CurriculoWEB/paginas/includes/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="row bg-danger">
+        <div class="col-4"></div>
+        <div class="col-4 text-center">
+            <img src="public/img/assinaturaAm.png" alt="" class="img-fluid p-4">
+        </div>
+        <div class="col-4"></div>
+    </div>
+</footer>
+
+<!-- jQuery -->
+<script src="public/js/jquery-3.5.1.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="public/css/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Meu css -->
+<script src="public/js/meuScript.js"></script>
+
+</body>
+</html>
